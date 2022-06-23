af575c6c28eea5b75a7741383778512c7f8490f6
Valscr <95771970+Valscr@users.noreply.github.com>
2022-06-23 11:47:18 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: vescaffr <vescaffr@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/21 16:32:27 by vescaffr          #+#    #+#              #
-#    Updated: 2022/06/21 16:58:29 by vescaffr         ###   ########.fr        #
+#    Updated: 2022/06/22 16:43:23 by vescaffr         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,10 +23,10 @@ RM		= rm -f
 CFLAGS = -Wall -Wextra -Werror
 
 .c.o:
-		${CC} ${CFLAGS} -Imlx_linux -O3 -c $< -o $@
+		${CC} ${CFLAGS} -c $< -o $@
 
 $(NAME): ${OBJS}
-		$(CC) $(OBJ) -Lmlx_linux -lmlx_Linux -Imlx_linux -lXext -lX11 -lm -lz -o $(NAME)
+		$(CC) $(OBJ) -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -o $(NAME)
 
 all:	${NAME}
 
--- so_long.c
+++ so_long.c
@@ -6,22 +6,127 @@
 /*   By: vescaffr <vescaffr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 17:21:55 by vescaffr          #+#    #+#             */
-/*   Updated: 2022/06/21 16:57:06 by vescaffr         ###   ########.fr       */
+/*   Updated: 2022/06/23 11:46:04 by vescaffr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <X11/X.h>
+#include <X11/keysym.h>
 #include <mlx.h>
 
-void	test(void)
-{
-	void	*mlx;
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 300
 
-	mlx = mlx_init();
-	mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_loop(mlx);
+#define MLX_ERROR 1
+
+#define RED_PIXEL 0xFF0000
+#define GREEN_PIXEL 0xFF00
+#define WHITE_PIXEL 0xFFFFFF
+
+typedef struct s_rect
+{
+	int	x;
+	int	y;
+	int width;
+	int height;
+	int color;
+}	t_rect;
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
+}	t_data;
+
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp; /* bits per pixel */
+	int		line_len;
+	int		endian;
+}	t_img;
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
+	return (0);
 }
+
+void	render_background(t_data *data, int color)
+{
+	int	i;
+	int	j;
+
+	if (data->win_ptr == NULL)
+		return ;
+	i = 0;
+	while (i < WINDOW_HEIGHT)
+	{
+		j = 0;
+		while (j < WINDOW_WIDTH)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, color);
+		++i;
+	}
+}
+
+int render_rect(t_data *data, t_rect rect)
+{
+	int	i;
+	int j;
+
+	if (data->win_ptr == NULL)
+		return (1);
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+		++i;
+	}
+	return (0);
+}
+
+int	render(t_data *data)
+{
+	render_background(data, WHITE_PIXEL);
+	render_rect(data, (t_rect){WINDOW_WIDTH / 2 - 50, WINDOW_HEIGHT / 2 - 50, 100, 100, GREEN_PIXEL});
+	/*render_rect(data, (t_rect){0, 0, 100, 100, RED_PIXEL});*/
+	return (0);
+}
+
+
 
 int	main(void)
 {
-	test();
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "my window");
+	if (data.win_ptr == NULL)
+	{
+		free(data.win_ptr);
+		return (MLX_ERROR);
+	}
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	mlx_get_data_addr(data.img.mlx_img, int *bits_per_pixel, int *size_line, int *endian);
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+
+	mlx_loop(data.mlx_ptr);
+
+	/* we will exit the loop if there's no window left, and execute this code */
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
